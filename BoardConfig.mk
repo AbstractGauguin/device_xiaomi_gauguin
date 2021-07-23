b496df8b457e24e0ebe9411ac2414857d5de0937
xSylla <syllamodder@gmail.com>
2021-07-23 19:09:03 +0200

    picasso: Inherit from common tree
    Signed-off-by: xSylla <syllamodder@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm7250-common
+include device/xiaomi/sm7250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/picasso
+
+BUILD_BROKEN_DUP_RULES := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/picasso_user_defconfig
+
+# Inherit from the proprietary version
+include vendor/xiaomi/picasso/BoardConfigVendor.mk
